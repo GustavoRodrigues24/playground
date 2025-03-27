93af5d194a7125dd1ba6f4c866433778ad41b288
Gustavo <gustavo12gargalac@gmail.com>
2025-03-26 21:11:23 -0300

    commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -35,7 +35,7 @@ describe('login', () => {
     cy.noticeHave('Por favor, digite sua senha para continuar.')
   })
 
-  it.only('Não deve logar sem o email e sem a senha', ()=> {
+  it('Não deve logar sem o email e sem a senha', ()=> {
     cy.login('', '')
     cy.noticeHave('Parece que você esqueceu de informar seu e-mail.')
   })
